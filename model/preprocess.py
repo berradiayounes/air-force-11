21354d98f24313eeb0d612a0232b9665dd113239
Yassine Omari <yassine.omari-betahi@student.ecp.fr>
2021-02-10 16:00:24 +0100

    Black

--- model/preprocess.py
+++ model/preprocess.py
@@ -5,21 +5,32 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
 from tqdm import tqdm
+
 tqdm.pandas()
+
 
 class Preprocessor:
     def preprocess_sentence(self, sentence):
-        sentence = "".join([c.lower() for c in sentence if c not in string.punctuation]).strip()
+        sentence = "".join(
+            [c.lower() for c in sentence if c not in string.punctuation]
+        ).strip()
 
-        sentence = " ".join([w for w in word_tokenize(sentence) if not w in stopwords.words("english")])
+        sentence = " ".join(
+            [
+                w
+                for w in word_tokenize(sentence)
+                if not w in stopwords.words("english")
+            ]
+        )
 
         stemmer = PorterStemmer()
         sentence = " ".join([stemmer.stem(w) for w in word_tokenize(sentence)])
-        
+
         return sentence
-    
+
     def preprocess(self, df, review_col):
         df = df.dropna(subset=[review_col])
-        df[f"{review_col}_preprocessed"] = df[review_col].progress_apply(self.preprocess_sentence)
+        df[f"{review_col}_preprocessed"] = df[review_col].progress_apply(
+            self.preprocess_sentence
+        )
         return df
-        
