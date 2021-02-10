147332d0048f2522c498694e0bbdff2619f40a83
Yassine Omari <yassine.omari-betahi@student.ecp.fr>
2021-02-10 16:01:02 +0100

    Black

--- scraping_scripts/trip_advisor_airlines.py
+++ scraping_scripts/trip_advisor_airlines.py
@@ -3,10 +3,12 @@ from selenium import webdriver
 import time
 import pandas as pd
 
-#Define driver
-driver = webdriver.Chrome("../../../chromedriver") #adapt based on your arborescence
+# Define driver
+driver = webdriver.Chrome(
+    "../../../chromedriver"
+)  # adapt based on your arborescence
 
-#Write function to iterate through pages
+# Write function to iterate through pages
 def _find_element_click(
     driver,
     by,
@@ -24,9 +26,13 @@ def _find_element_click(
     end_time = time.time() + 60
     while True:
         try:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight)"
+            )
             time.sleep(2)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight)"
+            )
             web_element = driver.find_element(by=by, value=expression)
             web_element.click()
             return True
@@ -36,35 +42,50 @@ def _find_element_click(
                 break
     return False
 
-#Get airlines
+
+# Get airlines
 url = "https://www.tripadvisor.com/Airlines"
 driver.get(url)
 page = BeautifulSoup(driver.page_source)
 
-#Get all airlines that are still operating
+# Get all airlines that are still operating
 airlines = []
 links = []
 review_count = []
 i = 0
-total_pages = int(page.find("div", {"class": "pageNumbers"}).find_all("span")[-1].text)
+total_pages = int(
+    page.find("div", {"class": "pageNumbers"}).find_all("span")[-1].text
+)
 for i in range(total_pages - 1):
     page = BeautifulSoup(driver.page_source)
 
-    for airline in page.find("div", {"class": "mainColumnContent"}).find_all("div", {"class": "airlineSummary"}):
+    for airline in page.find("div", {"class": "mainColumnContent"}).find_all(
+        "div", {"class": "airlineSummary"}
+    ):
         if "[no longer operating]" not in airline.text:
             airlines.append(airline.find("div").text)
             links.append(airline.find("a").get("href"))
             try:
-                review_string = airline.find("div", {"class": "airlineReviews"}).text.split(" review")[0].replace(",", "")
+                review_string = (
+                    airline.find("div", {"class": "airlineReviews"})
+                    .text.split(" review")[0]
+                    .replace(",", "")
+                )
                 review_count.append(int(review_string))
             except:
                 review_count.append(0)
-    
-    result = _find_element_click(driver, webdriver.common.by.By.CSS_SELECTOR, ".next")
+
+    result = _find_element_click(
+        driver, webdriver.common.by.By.CSS_SELECTOR, ".next"
+    )
     if result == False:
         break
         print("Failure to go to next page")
 
-#Write to csv
-airline_info = pd.DataFrame({'airlines': airlines, 'links': links, "review_count": review_count})
-airline_info.to_csv("../data/airline_links_tripadvisor.csv", sep=",", index = False)
+# Write to csv
+airline_info = pd.DataFrame(
+    {"airlines": airlines, "links": links, "review_count": review_count}
+)
+airline_info.to_csv(
+    "../data/airline_links_tripadvisor.csv", sep=",", index=False
+)
