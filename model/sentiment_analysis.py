7b2d6f46dcd79ee5fd3c0e03049e71075d6c5442
Yassine Omari <yassine.omari-betahi@student.ecp.fr>
2021-02-11 23:41:11 +0100

    Flexible topics

--- model/sentiment_analysis.py
+++ model/sentiment_analysis.py
@@ -1,20 +1,24 @@
+import inflect
 import numpy as np
 import pandas as pd
 import aspect_based_sentiment_analysis as absa
 from transformers import BertTokenizer
 from tqdm import tqdm
+import nltk
+
+nltk.download("punkt")
 
 
-def create_absa_pipeline(
+def _create_absa_pipeline(
     default_model=True, model_name="absa/classifier-rest-0.2"
 ):
     """
     Initialize the pipeline according to the desired model.
 
-    Input:
-      default_model: boolean, True if we want to use default absa model
-      model_name: str, 'absa/classifier-rest-0.2' or 'absa/classifier-lapt-0.2',
-                  rest for restaurants or lapt for laptops
+    Args:
+        default_model: boolean, True if we want to use default absa model
+        model_name: str, 'absa/classifier-rest-0.2' or 'absa/classifier-lapt-0.2',
+                    rest for restaurants or lapt for laptops
     """
 
     if default_model:
@@ -30,17 +34,22 @@ def create_absa_pipeline(
     return absa.Pipeline(model, tokenizer, professor, text_splitter)
 
 
-def get_aspects_for_review(review, all_aspects):
+def _get_aspects_for_review(review, all_aspects):
     """
     Select aspects that appear in the given review.
 
-    review: str, review in english
-    all_aspects: [str], list of all aspects of interest
+    Args:
+        review: str, review in english
+        all_aspects: [str], list of all aspects of interest
+
+    Returns:
+        aspects: [str], list of aspects of interest that appear
+                 in the review
     """
 
     aspects = []
     for aspect in all_aspects:
-        if aspect in review.lower():
+        if aspect in nltk.word_tokenize(review.lower()):
             aspects.append(aspect)
 
     return aspects
@@ -57,16 +66,29 @@ def get_sentiments(
     """
     Get sentiments on the given reviews regarding the given aspects.
 
-    default_model: boolean, True if we want to use default absa model
-    reviews: Pandas Dataframe, Dataframe with one "review" column indexed by integers
-    all_aspects: [str], list of all aspects of interest
-    model_name: str, 'absa/classifier-rest-0.2' or 'absa/classifier-lapt-0.2'
+    Args:
+        reviews: Pandas Dataframe, Dataframe with one "review" column indexed by integers
+        all_aspects: [str], list of all aspects of interest
+        default_model: boolean, True if we want to use default absa model
+        model_name: str, 'absa/classifier-rest-0.2' or 'absa/classifier-lapt-0.2'
+
+    Returns:
+        reviews: Pandas Dataframe, Dataframe with one "review" column and one column per
+                 aspect of interest containing NaN if aspect is not present in the review,
+                 +1 (resp. 0, resp. -1) if the sentiment is positive (resp. neutral,
+                 resp. negative)
     """
 
-    nlp = create_absa_pipeline(default_model, model_name)
+    nlp = _create_absa_pipeline(default_model, model_name)
 
     if verbose:
         print("Model created...")
+
+    # Add plural (resp. singular) form for each singular (resp. plural) form without duplicates
+    p = inflect.engine()
+    for aspect in all_aspects:
+        if p.plural(aspect) not in all_aspects:
+            all_aspects.append(p.plural(aspect))
 
     NaN = np.nan
     for aspect in all_aspects:
@@ -77,7 +99,7 @@ def get_sentiments(
 
     for i in tqdm(reviews.index):
         review = reviews.at[i, review_column]
-        aspects = get_aspects_for_review(review, all_aspects)
+        aspects = _get_aspects_for_review(review, all_aspects)
 
         if aspects != []:
             for task in nlp(review, aspects=aspects):
